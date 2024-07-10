b2e0cd77b516e5dcae5c71bdefc5ba89fbd839c9
supertgo <thiagoroberto2003@gmail.com>
2024-07-10 06:30:05 -0300

    show ci/cd

---
+++ frontend/src/components/ModalRemoveAccount/ModalRemoveAccount.tsx
@@ -0,0 +1,12 @@
+import * as S from './ModalRemoveAccount.styles'
+
+export type ModalRemoveAccountProps = {}
+
+export const ModalRemoveAccount = ({
+}: ModalRemoveAccountProps) => {
+  return (
+    <S.Wrapper>
+      ModalRemoveAccount
+    </S.Wrapper>
+  )
+}
